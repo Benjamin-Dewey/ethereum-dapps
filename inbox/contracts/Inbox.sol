03aeb29d22532329146830e7cc54f356131ae1f2
Benjamin Dewey <benjamin.m.dewey@gmail.com>
2018-02-03 15:14:40 -0500

    setup mocha testing script with a simple test

--- inbox/contracts/Inbox.sol
+++ inbox/contracts/Inbox.sol
@@ -1,13 +1,14 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.17; // solhint-disable-line
+
 
 contract Inbox {
-  string public message;
+    string public message;
 
-  function Inbox(string initialMessage) public {
-    message = initialMessage;
-  }
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
+    }
 
-  function setMessage(string newMessage) public {
-    message = newMessage;
-  }
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
 }
