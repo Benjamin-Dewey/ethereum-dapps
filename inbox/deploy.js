b46ed8602a669007f2f48866149defa3baa2ba8b
Benjamin Dewey <benjamin.m.dewey@gmail.com>
2018-02-07 18:02:12 -0500

    finish deploy code; succesfully deployed to Rinkeby

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -7,3 +7,20 @@ const network = 'https://rinkeby.infura.io/DXw2rqHt6I8SobHyuro6';
 
 const provider = new HDWalletProvider(mneumonic, network);
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  const mainAccount = accounts[0];
+
+  console.log('Attempting to deploy from account', mainAccount);
+
+  const abi = JSON.parse(interface);
+
+  const { options: { address } } = await new web3.eth.Contract(abi)
+    .deploy({ data: bytecode, arguments: ['Hi there'] })
+    .send({ gas: '1000000', from: mainAccount });
+
+  console.log('Contract deployed to', address);
+};
+
+deploy();
